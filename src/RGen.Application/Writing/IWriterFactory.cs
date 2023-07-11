849c1ce777df3c9a253ad357b943713aec5e3192
Björn <bjorn@sentiandream.com>
2023-07-11 10:23:57 +0200

    refactor: add register to interface

--- src/RGen.Application/Writing/IWriterFactory.cs
+++ src/RGen.Application/Writing/IWriterFactory.cs
@@ -1,7 +1,13 @@
-﻿namespace RGen.Application.Writing;
+﻿using System;
+
+
+namespace RGen.Application.Writing;
 
 public interface IWriterFactory
 {
+	public IWriterFactory Register<TOption>(Func<TOption, IWriter> factory)
+		where TOption : IWriterOptions;
+
 	IWriter Create<TOptions>(TOptions options)
 		where TOptions : IWriterOptions;
 }
